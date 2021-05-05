2da24ac80551e9141e32f5c5656d0544de30d67e
shunpei.kanbayashi <gkhandball1@gmail.com>
2021-05-05 13:54:53 +0900

    add desctop design and mobile-mee-menu

--- scripts/main.js
+++ scripts/main.js
@@ -5,10 +5,10 @@ document.addEventListener('DOMContentLoaded', function() {
   const cb = function(entries, observer) {
     entries.forEach(entry => {
       if(!entry.isIntersecting) {
-        console.log('outview');
+        //console.log('outview');
         $header.classList.add('torigger');
       } else {
-        console.log('inview');
+        //console.log('inview');
         $header.classList.remove('torigger');
       }
     });
@@ -25,9 +25,57 @@ document.addEventListener('DOMContentLoaded', function() {
     })
   }
 
-  const slide_el = document.querySelector('.cover-slide');
-  if (slide_el != null) {
+  const slide_els = document.querySelectorAll('.cover-slide');
+  if (slide_els != null) {
     const slide_io = new IntersectionObserver(slide_cb);
-    slide_io.observe(slide_el);
+    slide_els.forEach(slide_el => {
+      slide_io.observe(slide_el);
+    });
   }
+
+  const firstSwiper = new Swiper(".first-swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    autoplay: {
+      delay: 2000,
+    },
+    loop: true,
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+  });
+  const secondSwiper = new Swiper(".second-swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    autoplay: {
+      delay: 2000,
+      reverseDirection: true,
+    },
+    loop: true,
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+  });
+  const subhero_swiper = new Swiper(".subhero-swiper", {
+    slidesPerView: 4,
+    centeredSlides: true,
+    spaceBetween: 30,
+    grabCursor: true,
+    loop: true,
+    autoplay: {
+      delay: 1000
+    }
+  });
 });
